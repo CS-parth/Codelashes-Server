a32d46c11e63f042f80430308ca8928bfde8bae1
CS-parth <parth.vijy@gmail.com>
2024-06-24 07:53:37 +0530

    added mongodb atlas

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const stablishConnection = ()=>{
-  mongoose.connect('mongodb://localhost:27017/Codelashes?directConnection=true', {
+  mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
